e815fd0304895622146733352a9a4b7428a7f55f
CaidevOficial <falcone.facu@gmail.com>
2022-06-13 22:57:44 -0300

    Upd ⚙ - Ex_07

---
+++ Basic_07.py
@@ -0,0 +1,7 @@
+
+age = input('Dime tu edad: ')
+
+if age >= '18':
+    print(f'Tienes {age} años, por lo tanto eres mayor de edad.')
+else:
+    print(f'Tienes {age} años, por lo tanto eres menor de edad.')
