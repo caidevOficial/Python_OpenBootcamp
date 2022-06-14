8d68310347f33df3c7d47eb67012ca3ae099f308
CaidevOficial <falcone.facu@gmail.com>
2022-06-13 22:04:01 -0300

    Upd :gear: - First Ex.

---
+++ Basic_01.py
@@ -0,0 +1,10 @@
+from cgi import print_directory
+
+
+NAME = 'Facundo'
+print(
+    f'* Hola, soy {NAME}',
+    '* Estoy empezando el curso de python',
+    '* Espero aprender mucho',
+    sep='\n'
+)
